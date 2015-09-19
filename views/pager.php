bcd2ebd845825e373a268ef9cfb55dd4bbd4be95
BlueBrain <bluebrain.chef@googlemail.com>
2015-09-19 12:42:20 +0200

    Adding functions for small screen sizes Changes that make it possible to use the small-menu-function and also support smaller screen sizes. Created dashboard.png Created forums.png Created account.png Created character.png Created help.png Created logout.png Created mail.png added note where i got the icons from

--- views/pager.php
+++ views/pager.php
@@ -4,21 +4,25 @@
 	if (isset($_GET['page'])){
 		$page = strtolower($_GET['page']);
 	}
+    $minimizedmenu = 0;
+    if(isset($_GET["minimizedmenu"])){
+        $minimizedmenu = $_GET["minimizedmenu"];
+    }
 	$style = '';
 	$mysql = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 	$mysql->set_charset("utf8");
 	
-?>		<div class="menu<?php if ($style != ''){ echo " " . $style; } ?>">
+?>		<div class="menu<?php if ($style != ''){ echo " " . $style;} if($minimizedmenu){echo " minimizedmenu";} ?>">
 			<div class="logo pane"></div>
 			<ul class="nav">
-				<li><a class="button<?php echo isPage($page, 'dashboard'); ?>" href="?page=dashboard">Dashboard</a></li>
-				<li><a class="button<?php echo isPage($page, 'forums'); ?>" href="?page=forums">Forums</a></li>
-				<li><a class="button<?php echo isPage($page, 'account'); ?>" href="?page=account">Account</a></li>
-				<li><a class="button" href="?logout">Logout</a></li>
+				<li><a class="button dashboard-button<?php echo isPage($page, 'dashboard'); ?>" href="?page=dashboard">Dashboard</a></li>
+				<li><a class="button forums-button<?php echo isPage($page, 'forums'); ?>" href="?page=forums">Forums</a></li>
+				<li><a class="button account-button<?php echo isPage($page, 'account'); ?>" href="?page=account">Account</a></li>
+				<li><a class="button logout-button" href="?logout">Logout</a></li>
 			</ul>
 			<ul class="nav">
-				<li><a class="button<?php echo isPage($page, 'char'); ?>" href="?page=char">Character</a></li>
-				<li><a class="button<?php echo isPage($page, 'mail'); ?>" href="?page=mail">Mail</a></li>
+				<li><a class="button character-button<?php echo isPage($page, 'char'); ?>" href="?page=char">Character</a></li>
+				<li><a class="button mail-button<?php echo isPage($page, 'mail'); ?>" href="?page=mail">Mail</a></li>
 			</ul>
 			<?php if (isset($_SESSION['rank']) && $_SESSION['rank'] > 1){ ?><ul class="nav">
 				<li><span class="button title">Administration</span></li>
@@ -28,7 +32,7 @@
 				<li><a class="button<?php echo isPage($page, 'sessions'); ?>" href="?page=sessions">Sessions</a></li>
 			</ul>
 			<?php } ?><ul class="nav">
-				<li><a class="button<?php echo isPage($page, 'help'); ?>" href="?page=help">Help</a></li>
+				<li><a class="button help-button<?php echo isPage($page, 'help'); ?>" href="?page=help">Help</a></li>
 			</ul>
 			<ul class="nav">
 <?php
@@ -62,6 +66,9 @@
 		}
 	}
 ?>			</ul>
+            <ul class="nav">
+                <li><a class="button" id="menusizer" href="?page=<?php echo $page; ?>&minimizedmenu=<?php if($minimizedmenu){echo "0";}else{echo"1";} ?>"><?php if($minimizedmenu){echo ">";}else{echo"<";} ?></a></li>
+            </ul>
 		</div>
 		<div class="content-pane" style="padding-left: 180px; overflow: auto;">
 			<div style="padding: 40px;">
